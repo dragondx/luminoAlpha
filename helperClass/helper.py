d67f6371ab5916f1c147aa63dffeebb9d85ac2e1
Felix <felixkywong@gmail.com>
2020-04-14 05:15:18 +0800

    alpha v0.3

---
+++ helperClass/helper.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Service(Enum):
+    Context = 1
+    ObjectRec = 2
+    CharacterRec = 3
+    FaceRec = 4
+
---
+++ luminoManager.py
@@ -0,0 +1,32 @@
+class Lumino:
+
+    def __init__(self, verbose):
+        self.verbose = verbose
+        #  init all states here
+        
+
+
+    # function for bluetooth class lifecycle
+
+
+    # function for coral device
+
+
+    # function for cloud services
+
+
+    # functino for dialogflow
+
+
+    # function for camera
+
+
+    # function for tf
+
+
+
+
+    ## HELPER FUNCTIONS
+    # function check status -- returns True if no problem found
+
+    
